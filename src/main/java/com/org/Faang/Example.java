2264ba0146eef8b541a14619fe9df08d446dc3ab
Srikalpana02 <srikalpanansk@gmail.com>
2025-03-25 22:34:52 -0500

    Changed example file added to Kalpana Branch

--- src/main/java/com/org/Faang/Example.java
+++ src/main/java/com/org/Faang/Example.java
@@ -1,6 +1,6 @@
 package com.org.Faang;
 public class Example {
     public static void main(String[] args) {
-        System.out.println("I am Kalpana");
+        System.out.println("Faang Tech Git Class");
     }
 }
